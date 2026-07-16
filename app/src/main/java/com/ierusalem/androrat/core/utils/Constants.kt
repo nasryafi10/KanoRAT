caf32e9ef17345fc9c21bde983f6b652bd59d3ac
nasryafi10 <nasryafi10@gmail.com>
2026-07-17 01:04:52 +0300

    Add Constants for user settings and configuration
    Defines constants for user settings and application configuration.

---
+++ app/src/main/java/com/ierusalem/androrat/core/utils/Constants.kt
@@ -0,0 +1,18 @@
+package com.ierusalem.androrat.core.utils
+
+object Constants {
+    const val DATA_STORE_NAME = "user_settings_datastore"
+    const val DEFAULT_LOCALE = "en"
+    const val DEFAULT_LOGIN = "admin"
+    const val DEFAULT_PASSWORD = "password"
+    
+    const val PREFERENCE_LANGUAGE = "pref_language"
+    const val PREFERENCE_THEME = "pref_theme"
+    const val PREFERENCE_IS_LOGIN_REQUIRED = "pref_is_login_required"
+    const val PREFERENCE_LOGIN = "pref_login"
+    const val PREFERENCE_PASSWORD = "pref_password"
+
+    const val MIN_PASSWORD_LENGTH = 6
+    const val MINIMUM_PASSWORD_AND_LOGIN_LENGTH = 4
+    const val AGENT_URL = "http://127.0.0"
+}
